07d2bcb04aab348809683ab915fd795dae05dbda
Israel Viana <isra00@gmail.com>
2012-02-07 00:28:58 +0100

    Mejorado el pop-up de detalles de tarea y simplificado el diseño de las tareas

--- page_board.view.php
+++ page_board.view.php
@@ -3,13 +3,8 @@
     <div class="tarea" id="tarea-<?php echo $task['task_id'] ?>">
         <div class="dentro">
         
-            <div class="remove-task">
-                <a href="javascript:void(0)" class="remove-task-link" id="remove-<?php echo $task['task_id'] ?>">Eliminar</a>
-            </div>
-        
             <a class="img asignar" href="javascript:void(0)">
             <?php if (intval($task['assigned_to']) > 0) : ?>
-                <!--<img src="http://localhost/wordpress/wp-content/plugins/kanpress/static/isra.jpg" width="50" height="50" class="assigned-to" />-->
                 <?php echo get_avatar($task['assigned_to'], 50, null, $task['user_assigned']) ?>
             <?php else : ?>
                 No asignada
@@ -23,13 +18,52 @@
             <p>
                 <?php echo cortar_texto($task['description'], 80) ?>
                 
-                <?php if (strlen($task['description']) > 100) : ?>
                 <a href="javascript:void(0)" class="enlace-detalles" id="<?php echo $task['task_id'] ?>">[+info]</a>
                 
                 <div id="detalles-<?php echo $task['task_id'] ?>" class="tarea-detalles">
-                    <?php echo nl2br($task['description']) ?>
+                    <p class="asignacion">
+                    <?php if (empty($task['assigned_to'])) : ?>
+                        La tarea todavía no ha sido asignada a nadie.
+                    <?php else : ?>
+                        <?php echo get_avatar($task['assigned_to'], 50, null, $task['user_assigned']) ?>
+                        <span class="light">Asignada a</span>
+                        <br />
+                        <?php echo $task['user_assigned'] ?>
+                    <?php endif ?>
+                    </p>
+                    
+                    <div class="task-description">
+                        Descripción: <br />
+                        <textarea rows="4" cols="30"><?php echo $task['description'] ?></textarea>
+                    </div>
+                    
+                    <ul class="task-history">
+                        <li>
+                            Creada 
+                            <span><?php echo strtolower(hace_tiempo(strtotime($task['time_proposed']))) ?></span>
+                            por <span><?php echo $task['user_proposed'] ?></span>
+                        </li>
+                        <?php if (intval($task['assigned_to']) > 0) : ?>
+                        <li>
+                            Asignada
+                            <span><?php echo strtolower(hace_tiempo(strtotime($task['time_assigned']))) ?></span>
+                            a <span><?php echo $task['user_assigned'] ?></span>
+                        </li>
+                        <?php endif ?>
+                        <?php if (intval($task['time_done']) > 0) : ?>
+                        <li>
+                            Completada
+                            <span><?php echo strtolower(hace_tiempo(strtotime($task['time_done']))) ?></span>
+                        </li>
+                        <?php endif ?>
+                    </ul>
+                    
+                    <hr /> 
+                    
+                    <button type="button" name="assign" class="button-primary margen-arriba" id="btn-guardar">Guardar</button>
+                    o <a href="javascript:void(0)" class="cerrar-popup">cerrar sin guardar</a>
+                    o <a href="javascript:void(0)" class="remove-task-link" id="remove-<?php echo $task['task_id'] ?>">Eliminar tarea</a>
                 </div>
-                <?php endif ?>
             </p>
         </div>
         
@@ -39,7 +73,6 @@
             </div>
             <div class="meta">
                 <span class="creation-time"><?php echo hace_tiempo(strtotime($task['time_proposed'])) ?></span>
-                <a href="javascript:void(0)" class="detalles" id="detalles-<?php echo $task['task_id'] ?>">Detalles</a>
             </div>
         </div>
     </div>
@@ -76,16 +109,14 @@ jQuery(function() {
     
     //Pop-ups (genérico)
     $("#TB_closeWindowButton").click(function() {
-        //Muestra el overlay y el pop-up
-        $("#TB_overlay").hide();
-        $("#TB_window").hide();
+        cerrarPopup();
     });
     
     //Pop-up (detalles de tarea)
     $(".enlace-detalles").click(function() {
     
         //Título del pop-up
-        $("#TB_ajaxWindowTitle").html("Detalles del artículo propuesto");
+        $("#TB_ajaxWindowTitle").html($(this).parent().parent().find("h4").html());
         
         //Pone el contenido apropiado dentro del pop-up
         idTarea = $(this).attr("id");
@@ -94,6 +125,39 @@ jQuery(function() {
         //Muestra el overlay y el pop-up
         $("#TB_overlay").show();
         $("#TB_window").show();
+            
+        //Enlace "cerrar sin guardar"
+        $(".cerrar-popup").click(function() {
+            cerrarPopup();
+        });
+        
+        //Enlace "eliminar tarea"
+        $(".remove-task-link").click(function() {
+
+            if (confirm("¿Seguro que quieres eliminar esta tarea?\n¡No la podrás recuperar!")) {
+
+                //The element ID "remove-xxx" where xxx is the task ID. 
+                //7 is the length of "remove-"
+                taskId = $(this).attr("id").substr(7);
+
+                /* 
+                * For faster response, we remove the task from the HTML before we 
+                * get the confirmation from the server
+                */
+
+                //if (parseInt(response) == 1) {
+                    $("#tarea-" + taskId).hide('slow', function() {
+                        $(this).remove();
+                    });
+
+                    //Hide the popup also
+                    cerrarPopup();
+                //}
+
+                //Performs the AJAX request to remove the task
+                $.post(KANPRESS + '/ajax_remove_task.php', {task_id: taskId});
+            }
+        });
     });
     
     /**
@@ -122,6 +186,9 @@ jQuery(function() {
                 'user': $("#user").val()
             };
             
+            $("#TB_overlay").hide('fast');
+            $("#TB_window").hide('fast');
+            
             $.ajax({
                 type: 'POST',
                 url: KANPRESS + '/ajax_assign_task.php',
@@ -129,14 +196,9 @@ jQuery(function() {
                 data: postData,
                 success: function(response) {
                     $("#tarea-" + taskId + " .asignar").html(response);
-                    
-                    $("#TB_overlay").hide();
-                    $("#TB_window").hide();
                 },
                 error: function() {
                     
-                    $("#TB_overlay").hide();
-                    $("#TB_window").hide();
                 }
             });
         });
@@ -154,38 +216,9 @@ jQuery(function() {
         $("#resumen").focus();
     }
     
-    //Remove a task via AJAX
-    $(".remove-task-link").click(function() {
-    
-        if (confirm("¿Seguro que quieres eliminar esta tarea?\n¡No la podrás recuperar!")) {
-        
-            //The element ID "remove-xxx" where xxx is the task ID. 
-            //7 is the length of "remove-"
-            taskId = $(this).attr("id").substr(7);
-            
-            /* 
-             * For faster response, we remove the task from the HTML before we 
-             * get the confirmation from the server
-             */
-            
-            //if (parseInt(response) == 1) {
-                $("#tarea-" + taskId).hide('slow', function() {
-                    $(this).remove();
-                });
-            //}
-            
-            //Performs the AJAX request to remove the task
-            $.post(KANPRESS + '/ajax_remove_task.php', {task_id: taskId});
-        }
-    });
     
     //Pop-up (nueva tarea)
     $(".add-new-h2").click(mostrarPopupNuevoArticulo);
-    
-    <?php //Si el formulario no valida, mostramos el pop-up al inicio ?>
-    <?php if (!empty($validacion)) : ?>
-    mostrarPopupNuevoArticulo();
-    <?php endif ?>
     
     /**
      * Task drag-and-drop
@@ -230,9 +263,17 @@ jQuery(function() {
                 });
             }
         }
-        
-        //anteriorColumna
     });
+        
+    function cerrarPopup() {
+        $("#TB_overlay").hide();
+        $("#TB_window").hide();
+    }
+
+    <?php //Si el formulario no valida, mostramos el pop-up al inicio ?>
+    <?php if (!empty($validacion)) : ?>
+    mostrarPopupNuevoArticulo();
+    <?php endif ?>
 });
 </script>
 
