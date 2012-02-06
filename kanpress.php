5feaefebd6d2dccce7d0064a58097971bc3c6231
Israel Viana <isra00@gmail.com>
2012-02-06 18:38:08 +0100

    Mejores comentarios

--- kanpress.php
+++ kanpress.php
@@ -20,8 +20,9 @@ define('KANPRESS', plugins_url('', __FILE__));
  * Initialization
  */
 
-//Establece el huso horario para evitar problemas y los warning 5.3
+//Set the timezome. It avoids some warnings in PHP 5.3
 /** @todo Coger el del sistema */
+/** @todo ¿Es realmente necesario establecerlo? ¿No está ya establecido? */
 date_default_timezone_set('Europe/Madrid');
 setlocale(LC_ALL, 'es_ES.utf8');
 
@@ -50,3 +51,26 @@ function kanpress_create_admin_menu() {
 
 add_action('admin_menu', 'kanpress_create_admin_menu');
 
+
+/**
+ * Create or replace the DB schema table for store Kanpress tasks
+ * 
+ * @global type $wpdb   Wordpress database connection
+ */
+function kanpress_create_db_schema() {
+    global $wpdb;
+    /** @todo Implementar realmente! Esto es código de ejemplo!! */
+    /*$sql = $wpdb->prepare( "CREATE TABLE %s (
+            id mediumint(9) NOT NULL AUTO_INCREMENT,
+            time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+            name tinytext NOT NULL,
+            text text NOT NULL,
+            url VARCHAR(55) DEFAULT '' NOT NULL,
+            UNIQUE KEY id (id)
+            );", $table_name);
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);*/
+}
+
+register_activation_hook(__FILE__,'jal_install');
