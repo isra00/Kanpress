9d3e0203e06b8981b471ba84ea817c5ecdeb2a71
Israel Viana <isra00@gmail.com>
2012-02-07 13:05:30 +0100

    Guardar metadatos al crear el post

--- ajax_link_task.php
+++ ajax_link_task.php
@@ -19,20 +19,27 @@ if (current_user_can('edit_users')) {
         if ($taskId <= 0) { /** @todo Error 403 */ }
 
         $q_task = 'SELECT * FROM ' . TABLE_TASK . ' WHERE task_id = %d LIMIT 1';
-        $task = $wpdb->get_row($wpdb->prepare($select, $taskId), ARRAY_A);
+        $task = $wpdb->get_row($wpdb->prepare($q_task, $taskId), ARRAY_A);
 
         if (empty($task)) { /** @todo Error 403 */ }
 
+        //Creates the post
         $post_data = array(
             'post_title'    => ' ',
-            'post_category' => $task['term_id'],
+            'post_category' => array($task['term_id']),
             'post_content'  => '',
             'post_status'  => 'draft',
             'post_author'   => $task['assigned_to']
         );
-
         $post_id = wp_insert_post($post_data);
+        
+        //Insert post metadata with task data
+        /** @todo i18n the meta key? */
+        update_post_meta($post_id, 'task_id', $task['task_id']);
+        update_post_meta($post_id, 'task_summary', $task['summary']);
+        update_post_meta($post_id, 'task_description', $task['description']);
 
+        //Associate the task and the post
         $wpdb->update($wpdb->prefix . 'kanpress_task', 
                 array('post_id' => $post_id), 
                 array('task_id' => $taskId));
